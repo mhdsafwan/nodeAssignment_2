613ff6c54aa3d67e18f9213d3810245a877e8619
safwan <mohammedsafwan.int@niveussolutions.com>
2023-05-16 10:45:10 +0530

    Updated...

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+// ****************************************POST REQUSET START******************************************************
+//this will fetch the dummy data from json and displays
+app.get("/api/students", (req, res) => {
+  res.json(students);
+});
+
+//i have used thunder client vs-code extention to post a request and data is sent in body -> json and data will be rendering with existing...
+app.use(express.json());
+app.post("/api/students", (req, res) => {
+  const user = {
+    id: students.length + 1,
+    name: req.body.name,
+  };
+  students.push(user);
+  res.json(user);
+});
+// ****************************************POST REQUSET END******************************************************
+
+
+
+// ****************************************GET REQUSET START******************************************************
 //Additional.. if path : `localhost:3000`  then it will redirect to given path
 app.get("/", (req, res) => {
   res.statusCode = 302;
@@ -28,5 +49,4 @@ app.get("/api/:id", (req, res) => {
 
   res.json(info);
 });
-
-// ****************************************************************************************************************
+// ****************************************GET REQUSET END********************************************************
